f7be3964f53a2152894d42f4871ce038e20d5b28
Dhanashree Wani <203089528+cx-dhanashree-wani@users.noreply.github.com>
2025-07-14 17:15:51 +0530

    Update DBConnect.java

--- src/main/java/org/cysecurity/cspf/jvl/model/DBConnect.java
+++ src/main/java/org/cysecurity/cspf/jvl/model/DBConnect.java
@@ -20,5 +20,24 @@ import java.util.Properties;
  * @author breakthesec
  */
 public class DBConnect {
-    
+    public Connection connect(String path) throws IOException,ClassNotFoundException,SQLException
+    {
+        Properties properties=new Properties();
+        properties.load(new FileInputStream(path));
+        String dbuser=properties.getProperty("dbuser");
+         String dbpass = properties.getProperty("dbpass");
+       String dbfullurl = properties.getProperty("dburl")+properties.getProperty("dbname");
+       String jdbcdriver = properties.getProperty("jdbcdriver");
+            Connection con=null;
+          try
+                   {
+                    Class.forName(jdbcdriver);
+                    con= DriverManager.getConnection(dbfullurl,dbuser,dbpass);
+                    return con;
+                   }
+                   finally
+                    {
+                                 
+                    }      
+    }
 }
